66440adf017c437b64f1dd795660c094a23093d2
Annabelle Scripps <tzee8090362@outlook.com>
2026-03-24 12:21:34 +0900

    Update contract.ts

--- lib/contract.ts
+++ lib/contract.ts
@@ -1,53 +1,47 @@
 export const CONTRACT_ADDRESS = '0x4c50dc3b966937e42f43ca0b4e334a0e72464409';
-export const CHAIN_ID = 8453; // Base
+export const CHAIN_ID = 8453; // Base Mainnet
+
 export const BASE_BUILDER_CODE = 'bc_iejszfyu';
-export const BASE_ENCODED_STRING = '0x62635f69656a737a6679750b0080218021802180218021802180218021';
+export const BASE_ENCODED_STRING =
+  '0x62635f69656a737a6679750b0080218021802180218021802180218021';
 
 export const CONTRACT_ABI = [
   {
-    "inputs": [
-      { "internalType": "address", "name": "to", "type": "address" }
-    ],
-    "name": "mintBadge",
-    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    inputs: [{ internalType: 'address', name: 'to', type: 'address' }],
+    name: 'mintBadge',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
   },
   {
-    "inputs": [
-      { "internalType": "address", "name": "user", "type": "address" },
-      { "internalType": "string", "name": "action", "type": "string" }
+    inputs: [
+      { internalType: 'address', name: 'user', type: 'address' },
+      { internalType: 'string', name: 'action', type: 'string' },
     ],
-    "name": "recordAction",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: 'recordAction',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
   },
   {
-    "inputs": [
-      { "internalType": "address", "name": "user", "type": "address" }
-    ],
-    "name": "getUserActions",
-    "outputs": [{ "internalType": "string[]", "name": "", "type": "string[]" }],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [{ internalType: 'address', name: 'user', type: 'address' }],
+    name: 'getUserActions',
+    outputs: [{ internalType: 'string[]', name: '', type: 'string[]' }],
+    stateMutability: 'view',
+    type: 'function',
   },
   {
-    "inputs": [
-      { "internalType": "address", "name": "owner", "type": "address" }
-    ],
-    "name": "balanceOf",
-    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "stateMutability": "view",
-    "type": "function"
+    inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
   },
   {
-    "inputs": [
-      { "internalType": "uint256", "name": "tokenId", "type": "uint256" }
-    ],
-    "name": "tokenURI",
-    "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
-    "stateMutability": "view",
-    "type": "function"
-  }
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'tokenURI',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
 ] as const;
